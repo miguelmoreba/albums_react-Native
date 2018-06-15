034f44f6924edc38bb6ec7344ccf69472d3b1efc
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2018-06-15 14:08:50 +0100

    Added detail item

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Text} from 'react-native';
-import Card from './Card.js'
+import Card from './Card.js';
+import CardSection from './CardSection.js';
 
 const AlbumDetail = (props) => {
   return(
     <Card>
+      <CardSection>
       <Text>{props.album.title}</Text>
+      </CardSection>
     </Card>
   );
 };
