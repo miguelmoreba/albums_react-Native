d56e524afe2d09337bf7b93b9a155d3b9d4f7118
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2018-06-17 15:53:31 +0100

    Album artwork showing after updating AlbumDetail.js

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,57 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, Image} from 'react-native';
 import Card from './Card.js';
 import CardSection from './CardSection.js';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+  const {title, artist, thumbnail_image, image} = album;
   return(
     <Card>
       <CardSection>
-      <Text>{props.album.title}</Text>
+        <View style={styles.thumbnailContainerStyle}>
+          <Image
+            style={styles.thumbnailStyle}
+            source={{uri: thumbnail_image}}
+          />
+        </View>
+        <View style={styles.head}>
+          <Text style={styles.headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{uri: image}}
+          style={styles.imageStyle}
+        />
       </CardSection>
     </Card>
   );
+
 };
 
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+};
 export default AlbumDetail;
